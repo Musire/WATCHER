7176d96d6c319af6fd8f4b7f717fa0d85d23501c
Musire <Mcmusire@gmail.com>
2023-08-08 16:28:24 -0600

    imported the routes to use as middleware in the server

--- routes/api/goals.js
+++ routes/api/goals.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const goalController = require('../../controllers/GoaltController')
+const goalController = require('../../controllers/GoalController')
 
 router.post('/create_goal', goalController.create_goal);
 router.put('/edit_goal', goalController.edit_goal);
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const userRoute = require('./routes/api/users')
+const recordRoute = require('./routes/api/records')
+const goalRoute = require('./routes/api/goals')
+const accountRoute = require('./routes/api/accounts')
 
 // instantiate express server as app
 const express = require('express')
 const app = express()
 
-
 // cors middleware
 app.use(cors());
-
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,11 +21,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-
 // Bodyparser middleware
 app.use( bodyParser.urlencoded({ extended: false }) );
 app.use( bodyParser.json() );
-
 
 // Connect to MongoDB
 const db = process.env.MONGOURI;
@@ -31,6 +31,11 @@ mongoose.connect(db, { useNewUrlParser: true })
         .then(() => console.log("DB successfully connected"))
         .catch(err => console.log(err));
 
+
+app.use('/api/users', userRoute);
+app.use('/api/records', recordRoute);
+app.use('/api/goals', goalRoute);
+app.use('/api/accounts', accountRoute);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
